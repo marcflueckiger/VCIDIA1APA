f7dbdca2eb4108415d0aaa9c945a87787a26d39f
marcflueckiger <marc.flueckiger@student.ipso.ch>
2024-09-14 14:48:05 +0200

    v3

--- boot.sh
+++ boot.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+sudo supervisorctl stop fluga
+sudo service nginx stop
+rm -r __pycache__
+rm -r migrations/
+
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+
 flask db init
 flask db migrate
 flask db upgrade
 #pip install gunicorn
 gunicorn -b :5000 --access-logfile - --error-logfile - fluga:app
 
+sudo supervisorctl start fluga
+sudo service nginx start
 
-echo DATABASE_URL=mysql+pymysql://user:password@localhost/fluga >.env 
+#echo DATABASE_URL=mysql+pymysql://user:password@localhost/fluga >.env 
--- fluga.py
+++ fluga.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Message, Notification, Task
+from app.models import User, Post, Message, Notification, Task, Rating
 
 app = create_app()
 cli.register(app)
@@ -8,7 +8,7 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
+            'Notification': Notification, 'Task': Task, 'Rating': Rating}
 
 if __name__ == "__main__":
     app.run(debug=True)
